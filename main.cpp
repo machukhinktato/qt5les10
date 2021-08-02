033d38faf3a51fdd22e0a76bcbac92252eb63cfe
machukhinktato <tarabrinmv@gmail.com>
2021-08-02 19:51:19 +0300

    combobox done

--- main.cpp
+++ main.cpp
@@ -2,14 +2,17 @@
 #include <QTextStream>
 #include "label.h"
 #include "slider.h"
+#include "combobox.h"
+
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
 //    Label window;
-    Slider window;
+//    Slider window;
+    ComboBoxEx window;
 
-
+    window.resize(300,150);
     window.setWindowTitle("machukhinktato rule the ways");
     window.show();
     return app.exec();
